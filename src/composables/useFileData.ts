656bd84d3f1bf3f3ade595e558a591725c1cc9a0
Aram Becker <becker.aram@gmail.com>
2022-05-04 10:21:32 +0200

    feat: allow skipping lines when sampling files

--- src/composables/useFileData.ts
+++ src/composables/useFileData.ts
@@ -2,13 +2,13 @@ import fetch from "node-fetch";
 import { readFile } from "fs";
 import { Uri } from "vscode";
 
-export default function useFileData(uri: Uri) {
+export default function useFileData(uri: Uri, skipLines: number = 0) {
   let content: Promise<string>;
 
   switch (uri.scheme) {
     case "file":
       content = new Promise((resolve, reject) =>
-        readFile(uri.fsPath, { encoding: "utf8" }, (err, data) => (err ? reject(err) : resolve(data)))
+        readFile(uri.path, { encoding: "utf8" }, (err, data) => (err ? reject(err) : resolve(data)))
       );
       break;
     case "http":
@@ -19,7 +19,7 @@ export default function useFileData(uri: Uri) {
   }
 
   const lines = content.then((data) => {
-    const textLines = data.split(/\r?\n/);
+    const textLines = data.split(/\r?\n/).slice(skipLines);
     return textLines.map((text, line) => ({ text, line, length: text.length }));
   });
 
